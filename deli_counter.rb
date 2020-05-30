c50a79d8f19c293cde1a15cf081f7a7cbfbc77e0
korey ko <koreykendrick1@gmail.com>
2020-05-30 02:35:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def line(katz_deli)
+new_array = []
+  if katz_deli == []
+    puts "The line is currently empty."
+else
+  put = 1
+  sentence  = "The line is currently:"
+  new_array << sentence
+katz_deli.each do |name|
+new_array << ("#{put}. #{name}")
+put += 1
+
+ end 
+ puts new_array.join(" ")
+end 
+end
+
+def take_a_nuber(katz_deli)
+end
